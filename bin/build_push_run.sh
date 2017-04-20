a21bca4688fe096f3e28844064757db5c0c5fa25
xiaomagexiao <mayanjunchina@163.com>
2017-04-20 22:31:53 +0800

    修改脚本

--- bin/build_push_run.sh
+++ bin/build_push_run.sh
@@ -1,7 +1,7 @@
 pwd=`pwd`
 
 libname="android_zygote_injection-master"
-
+soname="android_dex_injection-master.so"
 echo "cd ../${libname}/jni"
 cd ../${libname}/jni
 ndk-build
@@ -12,7 +12,8 @@ pwd
 cp -f ../${libname}/libs/armeabi-v7a/${libname} .
 
 adb push ${libname} /data/local/tmp/${libname}
-
+adb push ${soname} /data/local/tmp/${soname}
+adb shell chmod 777 /data/local/tmp/${soname}
 adb shell chmod 777 /data/local/tmp/${libname}
 
 adb shell /data/local/tmp/${libname}
